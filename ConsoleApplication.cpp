ca5d0d3849d19aaa3d60c0f4706da356b063c160
Itai Danielli <Danielli.Itai@gmail.com>
2021-05-04 08:08:12 +0300

    add code documentation remove baselib

--- ConsoleApplication.cpp
+++ ConsoleApplication.cpp
@@ -10,7 +10,8 @@
 
 
 
-
+//Echo demonstrates a command execution function.
+// the command prints the parameters it receives.
 bool CommandEcho(std::vector<std::string> params)
 {
    for (auto it = params.begin(); it<params.end(); it++) {
@@ -21,19 +22,31 @@ bool CommandEcho(std::vector<std::string> params)
 }
 
 
-
+// Main function.
 int main()
 {
+   // Indicates whther to continue reading input.
    bool running = true;
+
+   // Initialize the commands object.
    Commands* pCommands = CommandsInit();
+
+   // Assign the command function to the command text.
    CommandAdd(pCommands, CMD_ECHO, CommandEcho);
    
+   // Store user input text.
    std::string command_line;
+
+   // Continues loop.
    while (running)
    {
+      // Print command promped '>'
       std::cout << CMD_PROMPED;
-      getline(std::cin, command_line);
 
+      // Get console command text"
+      getline(std::cin, command_line);
+ 
+      // Call for command execution.
       running = CommandExec(pCommands, command_line);
    }
 
